587ee4edc0fa6c67a15365bc225aebc1eec76b3f
Paul Adams <ghusiyy@gmail.com>
2022-07-12 23:27:47 -0700

    add simple edit mode

--- src/base-receipt.ts
+++ src/base-receipt.ts
@@ -29,35 +29,49 @@ export class BaseReceipt extends LitElement {
       border: 1px dashed gray;
       margin: 18px 0 10px;
     }
+    .Edit-mode {
+      margin: 4px 0 4px 0;
+      padding: 4px 0 4px 0;
+      box-shadow: 0px 0px 2px 2px #1996fd;
+    }
   `;
   @property()
   data: ReceiptLineItems = [];
 
   @property()
+  unsavedChanges: Boolean = false;
+
+  @property()
   heading: String = "Bedrock Supermarket";
 
-  private static lineItemEl(t: TemplateResult): TemplateResult {
-    function _edit() {
-      console.log("edit");
-    }
-    function _save() {
-      console.log("save");
-    }
-    return html`<div
-      class="Receipt-line-item"
-      @dblclick="${() => _edit()}"
-      @click="${() => _save()}"
-    >
+  private edit(target: any) {
+    // TODO: add ability to edit any target passed in
+    this.unsavedChanges = !this.unsavedChanges;
+  }
+
+  private save(target?: any) {
+    // TODO: commit change to target
+    this.unsavedChanges = false;
+  }
+  private cancel(target?: any) {
+    this.unsavedChanges = false;
+  }
+  private lineItemEl(
+    t: TemplateResult,
+    className: string = "Receipt-line-item",
+    target?: any
+  ): TemplateResult {
+    return html`<div class=${className} @dblclick="${() => this.edit(target)}">
       ${t}
     </div>`;
   }
 
-  static receiptLineItem(lineItem: ReceiptLineItem) {
+  receiptLineItem(lineItem: ReceiptLineItem) {
     return this.lineItemEl(html`<div>${lineItem.description}</div>
       <div class="Right-align">${lineItem.price}</div>`);
   }
 
-  static totalLineItem(total: number) {
+  totalLineItem(total: number) {
     return this.lineItemEl(html` <div>Total</div>
       <div class="Right-align">${total}</div>`);
   }
@@ -75,12 +89,18 @@ export class BaseReceipt extends LitElement {
 
     return html`<article class="Base-receipt">
       <section class="Wrapper">
-        <h3>${this.heading}</h3>
+        ${this.unsavedChanges
+          ? html`<div class="Edit-mode">
+              <input value=${this.heading} /><button @click=${this.cancel}>
+                cancel</button
+              ><button @click=${this.save}>save</button>
+            </div>`
+          : this.lineItemEl(html`<h3>${this.heading}</h3>`, "", this.heading)}
         ${this.data.map((item) => {
-          return BaseReceipt.receiptLineItem(item);
+          return this.receiptLineItem(item);
         })}
         <div class="Divider"></div>
-        ${BaseReceipt.totalLineItem(this.total())}
+        ${this.totalLineItem(this.total())}
       </section>
     </article>`;
   }
